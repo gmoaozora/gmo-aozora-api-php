ccc81612a2647ffb70bc43075d241d18d05a26da
GMO Aozora Net Bank, Ltd <50660041+gmoaozora-sys@users.noreply.github.com>
2020-02-13 16:36:28 +0900

    add function for VisaTransation (#9)

--- corporate/docs/Api/AccountApi.md
+++ corporate/docs/Api/AccountApi.md
@@ -8,7 +8,7 @@ Method | HTTP request | Description
 [**accountsUsingGET**](AccountApi.md#accountsUsingGET) | **GET** /accounts | 口座一覧照会
 [**balancesUsingGET**](AccountApi.md#balancesUsingGET) | **GET** /accounts/balances | 残高照会
 [**transactionsUsingGET**](AccountApi.md#transactionsUsingGET) | **GET** /accounts/transactions | 入出金明細照会
-
+[**visaTransactionsUsingGET**](AccountApi.md#visaTransactionsUsingGET) | **GET** /accounts/visa-transactions | Visaデビット取引明細照会
 
 # **accountsDepositTransactionsUsingGET**
 > \Ganb\Corporate\Client\Model\DepositTransactionsResponse accountsDepositTransactionsUsingGET($account_id, $x_access_token, $date_from, $date_to, $next_item_key)
@@ -269,6 +269,92 @@ Name | Type | Description  | Notes
 ### Return type
 
 [**\Ganb\Corporate\Client\Model\TransactionsResponse**](../Model/TransactionsResponse.md)
+
+### Authorization
+
+No authorization required
+
+### HTTP request headers
+
+ - **Content-Type**: application/json;charset=UTF-8
+ - **Accept**: application/json;charset=UTF-8
+
+[[Back to top]](#) [[Back to API list]](../../README.md#documentation-for-api-endpoints) [[Back to Model list]](../../README.md#documentation-for-models) [[Back to README]](../../README.md)
+
+# **visaTransactionsUsingGET**
+> \Ganb\Corporate\Client\Model\VisaTransactionsResponse visaTransactionsUsingGET($account_id, $x_access_token, $date_from, $date_to, $next_item_key)
+
+### Visaデビット取引明細照会
+
+指定した円普通預金口座のVisaデビット取引明細情報を照会します
+
+### 詳細説明
+
+#### 対象科目
+
+* 円普通預金口座かつ、Visaデビットを現時点で保有している口座
+
+#### 取得上限件数
+
+* 500件
+* 取得できる明細数が500に満たないときは取得できる明細のみを返却します
+* 取得できる明細が存在しない場合は「200：OK」とし明細を返却しません
+* ただし、1回の検索で総件数が99,999件を超える照会はできません。それ以上の場合は「400 Bad Request」を返却します
+
+#### ページング
+* 2ページ目以降を照会する際は、初回と同じリクエスト内容に、初回レスポンスの次明細キーを追加してリクエストしてください
+
+#### ソート順
+* 取引の降順
+
+#### 対象期間
+
+日本語名     | &#9312; | &#9313; | &#9314; | &#9315;
+:----|:----:|:----:|:----:|:----:
+対象期間From | × | ○ | × | ○
+対象期間To   | × | × | ○ | ○
+* &#9312;の場合: 当日分のVisaデビット取引明細を返却
+* &#9313;の場合: 対象期間From ～ 当日までのVisaデビット取引明細を返却
+* &#9314;の場合: 取引初回 ～ 対象期間ToまでのVisaデビット取引明細を返却
+* &#9315;の場合: 対象期間From ～ 対象期間ToまでのVisaデビット取引明細を返却
+
+### Example
+```php
+<?php
+require_once(__DIR__ . '/vendor/autoload.php');
+
+$apiInstance = new Ganb\Corporate\Client\Api\AccountApi(
+    // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
+    // This is optional, `GuzzleHttp\Client` will be used as default.
+    new GuzzleHttp\Client()
+);
+$account_id = "account_id_example"; // string | 口座ID 半角英数字 口座を識別するID  科目コードが以下の場合のみ受け付けます ・01=普通預金（有利息） ・02=普通預金（決済用）  minLength: 12 maxLength: 29
+$x_access_token = "x_access_token_example"; // string | アクセストークン  minLength: 1 maxLength: 128
+$date_from = "date_from_example"; // string | 対象期間From 半角文字 YYYY-MM-DD形式  minLength: 10 maxLength: 10
+$date_to = "date_to_example"; // string | 対象期間To 半角文字 YYYY-MM-DD形式 対象期間Fromと対象期間Toを指定する場合は、対象期間From≦対象期間Toとし、それ以外は「400 Bad Request」を返却  minLength: 10 maxLength: 10
+$next_item_key = "next_item_key_example"; // string | 次明細キー 半角数字 初回要求時は未設定 初回応答で次明細キーが「true」の場合、返却された同項目を2回目以降に設定  minLength: 1 maxLength: 24
+
+try {
+    $result = $apiInstance->visaTransactionsUsingGET($account_id, $x_access_token, $date_from, $date_to, $next_item_key);
+    print_r($result);
+} catch (Exception $e) {
+    echo 'Exception when calling AccountApi->visaTransactionsUsingGET: ', $e->getMessage(), PHP_EOL;
+}
+?>
+```
+
+### Parameters
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **account_id** | **string**| 口座ID 半角英数字 口座を識別するID 科目コードが以下の場合のみ受け付けます ・01&#x3D;普通預金（有利息） ・02&#x3D;普通預金（決済用）  minLength: 12 maxLength: 29  | 
+ **x_access_token** | **string**| アクセストークン  minLength: 1 maxLength: 128  | 
+ **date_from** | **string**| 対象期間From 半角文字 YYYY-MM-DD形式  minLength: 10 maxLength: 10  | [optional] 
+ **date_to** | **string**| 対象期間To 半角文字 YYYY-MM-DD形式 対象期間Fromと対象期間Toを指定する場合は、対象期間From≦対象期間Toとし、それ以外は「400 Bad Request」を返却  minLength: 10 maxLength: 10  |  [optional] 
+ **next_item_key** | **string**| 次明細キー 半角数字 初回要求時は未設定 初回応答で次明細キーが「true」の場合、返却された同項目を2回目以降に設定  minLength: 1 maxLength: 24  | [optional] 
+
+### Return type
+
+[**\Ganb\Corporate\Client\Model\VisaTransactionsResponse**](../Model/VisaTransactionsResponse.md)
 
 ### Authorization
 
--- corporate/lib/Api/AccountApi.php
+++ corporate/lib/Api/AccountApi.php
@@ -1614,4 +1614,400 @@ class AccountApi
 
         return $options;
     }
+
+    /**
+     * Operation visaTransactionsUsingGET
+     *
+     * Visaデビット取引明細照会
+     *
+     * @param  string $account_id 口座ID 半角英数字 口座を識別するID 科目コードが以下の場合のみ受け付けます ・01&#x3D;普通預金（有利息） ・02&#x3D;普通預金（決済用）  minLength: 12 maxLength: 29 (required)
+     * @param  string $x_access_token アクセストークン  minLength: 1 maxLength: 128 (required)
+     * @param  string $date_from 対象期間From 半角文字 YYYY-MM-DD形式  minLength: 10 maxLength: 10 (optional)
+     * @param  string $date_to 対象期間To 半角文字 YYYY-MM-DD形式 対象期間Fromと対象期間Toを指定する場合は、対象期間From≦対象期間Toとし、それ以外は「400 Bad Request」を返却  minLength: 10 maxLength: 10 (optional)
+     * @param  string $next_item_key 次明細キー 半角数字 初回要求時は未設定 初回応答で次明細キーが「true」の場合、返却された同項目を2回目以降に設定  minLength: 1 maxLength: 24 (optional)
+     *
+     * @throws \Ganb\Corporate\Client\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return \Ganb\Corporate\Client\Model\VisaTransactionsResponse
+     */
+    public function visaTransactionsUsingGET($account_id, $x_access_token, $date_from = null, $date_to = null, $next_item_key = null)
+    {
+        list($response) = $this->visaTransactionsUsingGETWithHttpInfo($account_id, $x_access_token, $date_from, $date_to, $next_item_key);
+        return $response;
+    }
+
+    /**
+     * Operation visaTransactionsUsingGETWithHttpInfo
+     *
+     * Visaデビット取引明細照会
+     *
+     * @param  string $account_id 口座ID 半角英数字 口座を識別するID 科目コードが以下の場合のみ受け付けます ・01&#x3D;普通預金（有利息） ・02&#x3D;普通預金（決済用）  minLength: 12 maxLength: 29 (required)
+     * @param  string $x_access_token アクセストークン  minLength: 1 maxLength: 128 (required)
+     * @param  string $date_from 対象期間From 半角文字 YYYY-MM-DD形式  minLength: 10 maxLength: 10 (optional)
+     * @param  string $date_to 対象期間To 半角文字 YYYY-MM-DD形式 対象期間Fromと対象期間Toを指定する場合は、対象期間From≦対象期間Toとし、それ以外は「400 Bad Request」を返却  minLength: 10 maxLength: 10 (optional)
+     * @param  string $next_item_key 次明細キー 半角数字 初回要求時は未設定 初回応答で次明細キーが「true」の場合、返却された同項目を2回目以降に設定  minLength: 1 maxLength: 24 (optional)
+     *
+     * @throws \Ganb\Corporate\Client\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return array of \Ganb\Corporate\Client\Model\VisaTransactionsResponse, HTTP status code, HTTP response headers (array of strings)
+     */
+    public function visaTransactionsUsingGETWithHttpInfo($account_id, $x_access_token, $date_from = null, $date_to = null, $next_item_key = null)
+    {
+        $returnType = '\Ganb\Corporate\Client\Model\VisaTransactionsResponse';
+        $request = $this->visaTransactionsUsingGETRequest($account_id, $x_access_token, $date_from, $date_to, $next_item_key);
+
+        try {
+            $options = $this->createHttpClientOption();
+            try {
+                $response = $this->client->send($request, $options);
+            } catch (RequestException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    $e->getCode(),
+                    $e->getResponse() ? $e->getResponse()->getHeaders() : null,
+                    $e->getResponse() ? $e->getResponse()->getBody()->getContents() : null
+                );
+            }
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode < 200 || $statusCode > 299) {
+                throw new ApiException(
+                    sprintf(
+                        '[%d] Error connecting to the API (%s)',
+                        $statusCode,
+                        $request->getUri()
+                    ),
+                    $statusCode,
+                    $response->getHeaders(),
+                    $response->getBody()
+                );
+            }
+
+            $responseBody = $response->getBody();
+            if ($returnType === '\SplFileObject') {
+                $content = $responseBody; //stream goes to serializer
+            } else {
+                $content = $responseBody->getContents();
+                if ($returnType !== 'string') {
+                    $content = json_decode($content);
+                }
+            }
+
+            return [
+                ObjectSerializer::deserialize($content, $returnType, []),
+                $response->getStatusCode(),
+                $response->getHeaders()
+            ];
+
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+                case 200:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\VisaTransactionsResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 400:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 401:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 403:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 404:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 405:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 406:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 411:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 412:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 415:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 500:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 503:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+                case 504:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Ganb\Corporate\Client\Model\ErrorResponse',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+            }
+            throw $e;
+        }
+    }
+
+    /**
+     * Operation visaTransactionsUsingGETAsync
+     *
+     * Visaデビット取引明細照会
+     *
+     * @param  string $account_id 口座ID 半角英数字 口座を識別するID 科目コードが以下の場合のみ受け付けます ・01&#x3D;普通預金（有利息） ・02&#x3D;普通預金（決済用）  minLength: 12 maxLength: 29 (required)
+     * @param  string $x_access_token アクセストークン  minLength: 1 maxLength: 128 (required)
+     * @param  string $date_from 対象期間From 半角文字 YYYY-MM-DD形式  minLength: 10 maxLength: 10 (optional)
+     * @param  string $date_to 対象期間To 半角文字 YYYY-MM-DD形式 対象期間Fromと対象期間Toを指定する場合は、対象期間From≦対象期間Toとし、それ以外は「400 Bad Request」を返却  minLength: 10 maxLength: 10 (optional)
+     * @param  string $next_item_key 次明細キー 半角数字 初回要求時は未設定 初回応答で次明細キーが「true」の場合、返却された同項目を2回目以降に設定  minLength: 1 maxLength: 24 (optional)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function visaTransactionsUsingGETAsync($account_id, $x_access_token, $date_from = null, $date_to = null, $next_item_key = null)
+    {
+        return $this->visaTransactionsUsingGETAsyncWithHttpInfo($account_id, $x_access_token, $date_from, $date_to, $next_item_key)
+            ->then(
+                function ($response) {
+                    return $response[0];
+                }
+            );
+    }
+
+    /**
+     * Operation visaTransactionsUsingGETAsyncWithHttpInfo
+     *
+     * Visaデビット取引明細照会
+     *
+     * @param  string $account_id 口座ID 半角英数字 口座を識別するID 科目コードが以下の場合のみ受け付けます ・01&#x3D;普通預金（有利息） ・02&#x3D;普通預金（決済用）  minLength: 12 maxLength: 29 (required)
+     * @param  string $x_access_token アクセストークン  minLength: 1 maxLength: 128 (required)
+     * @param  string $date_from 対象期間From 半角文字 YYYY-MM-DD形式  minLength: 10 maxLength: 10 (optional)
+     * @param  string $date_to 対象期間To 半角文字 YYYY-MM-DD形式 対象期間Fromと対象期間Toを指定する場合は、対象期間From≦対象期間Toとし、それ以外は「400 Bad Request」を返却  minLength: 10 maxLength: 10 (optional)
+     * @param  string $next_item_key 次明細キー 半角数字 初回要求時は未設定 初回応答で次明細キーが「true」の場合、返却された同項目を2回目以降に設定  minLength: 1 maxLength: 24 (optional)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function visaTransactionsUsingGETAsyncWithHttpInfo($account_id, $x_access_token, $date_from = null, $date_to = null, $next_item_key = null)
+    {
+        $returnType = '\Ganb\Corporate\Client\Model\VisaTransactionsResponse';
+        $request = $this->visaTransactionsUsingGETRequest($account_id, $x_access_token, $date_from, $date_to, $next_item_key);
+
+        return $this->client
+            ->sendAsync($request, $this->createHttpClientOption())
+            ->then(
+                function ($response) use ($returnType) {
+                    $responseBody = $response->getBody();
+                    if ($returnType === '\SplFileObject') {
+                        $content = $responseBody; //stream goes to serializer
+                    } else {
+                        $content = $responseBody->getContents();
+                        if ($returnType !== 'string') {
+                            $content = json_decode($content);
+                        }
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, $returnType, []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
+                },
+                function ($exception) {
+                    $response = $exception->getResponse();
+                    $statusCode = $response->getStatusCode();
+                    throw new ApiException(
+                        sprintf(
+                            '[%d] Error connecting to the API (%s)',
+                            $statusCode,
+                            $exception->getRequest()->getUri()
+                        ),
+                        $statusCode,
+                        $response->getHeaders(),
+                        $response->getBody()
+                    );
+                }
+            );
+    }
+
+    /**
+     * Create request for operation 'visaTransactionsUsingGET'
+     *
+     * @param  string $account_id 口座ID 半角英数字 口座を識別するID 科目コードが以下の場合のみ受け付けます ・01&#x3D;普通預金（有利息） ・02&#x3D;普通預金（決済用）  minLength: 12 maxLength: 29 (required)
+     * @param  string $x_access_token アクセストークン  minLength: 1 maxLength: 128 (required)
+     * @param  string $date_from 対象期間From 半角文字 YYYY-MM-DD形式  minLength: 10 maxLength: 10 (optional)
+     * @param  string $date_to 対象期間To 半角文字 YYYY-MM-DD形式 対象期間Fromと対象期間Toを指定する場合は、対象期間From≦対象期間Toとし、それ以外は「400 Bad Request」を返却  minLength: 10 maxLength: 10 (optional)
+     * @param  string $next_item_key 次明細キー 半角数字 初回要求時は未設定 初回応答で次明細キーが「true」の場合、返却された同項目を2回目以降に設定  minLength: 1 maxLength: 24 (optional)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Psr7\Request
+     */
+    protected function visaTransactionsUsingGETRequest($account_id, $x_access_token, $date_from = null, $date_to = null, $next_item_key = null)
+    {
+        // verify the required parameter 'account_id' is set
+        if ($account_id === null || (is_array($account_id) && count($account_id) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $account_id when calling visaTransactionsUsingGET'
+            );
+        }
+        // verify the required parameter 'x_access_token' is set
+        if ($x_access_token === null || (is_array($x_access_token) && count($x_access_token) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $x_access_token when calling visaTransactionsUsingGET'
+            );
+        }
+
+        $resourcePath = '/accounts/visa-transactions';
+        $formParams = [];
+        $queryParams = [];
+        $headerParams = [];
+        $httpBody = '';
+        $multipart = false;
+
+        // query params
+        if ($account_id !== null) {
+            $queryParams['accountId'] = ObjectSerializer::toQueryValue($account_id);
+        }
+        // query params
+        if ($date_from !== null) {
+            $queryParams['dateFrom'] = ObjectSerializer::toQueryValue($date_from);
+        }
+        // query params
+        if ($date_to !== null) {
+            $queryParams['dateTo'] = ObjectSerializer::toQueryValue($date_to);
+        }
+        // query params
+        if ($next_item_key !== null) {
+            $queryParams['nextItemKey'] = ObjectSerializer::toQueryValue($next_item_key);
+        }
+        // header params
+        if ($x_access_token !== null) {
+            $headerParams['x-access-token'] = ObjectSerializer::toHeaderValue($x_access_token);
+        }
+
+
+        // body params
+        $_tempBody = null;
+
+        if ($multipart) {
+            $headers = $this->headerSelector->selectHeadersForMultipart(
+                ['application/json;charset=UTF-8']
+            );
+        } else {
+            $headers = $this->headerSelector->selectHeaders(
+                ['application/json;charset=UTF-8'],
+                ['application/json;charset=UTF-8']
+            );
+        }
+
+        // for model (json/xml)
+        if (isset($_tempBody)) {
+            // $_tempBody is the method argument, if present
+            $httpBody = $_tempBody;
+            // \stdClass has no __toString(), so we should encode it manually
+            if ($httpBody instanceof \stdClass && $headers['Content-Type'] === 'application/json') {
+                $httpBody = \GuzzleHttp\json_encode($httpBody);
+            }
+        } elseif (count($formParams) > 0) {
+            if ($multipart) {
+                $multipartContents = [];
+                foreach ($formParams as $formParamName => $formParamValue) {
+                    $multipartContents[] = [
+                        'name' => $formParamName,
+                        'contents' => $formParamValue
+                    ];
+                }
+                // for HTTP post (form)
+                $httpBody = new MultipartStream($multipartContents);
+
+            } elseif ($headers['Content-Type'] === 'application/json') {
+                $httpBody = \GuzzleHttp\json_encode($formParams);
+
+            } else {
+                // for HTTP post (form)
+                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+            }
+        }
+
+
+        $defaultHeaders = [];
+        if ($this->config->getUserAgent()) {
+            $defaultHeaders['User-Agent'] = $this->config->getUserAgent();
+        }
+
+        $headers = array_merge(
+            $defaultHeaders,
+            $headerParams,
+            $headers
+        );
+
+        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        return new Request(
+            'GET',
+            $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
+            $headers,
+            $httpBody
+        );
+    }
 }
